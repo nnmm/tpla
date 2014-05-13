75a0790bd422454f3fc011e7463a62c0531b074d
Nikolai Morin <nikolaimorin@gmail.com>
2014-05-13 16:04:10 +0200

    wip

--- example.js
+++ example.js
@@ -1,14 +1,38 @@
 var jsonObject = {
-    "title": "Kuckucksuhr",
-    "text": "Eine Kuckucksuhr hängt an der Wand und tickt schon seit 250 Jahren vor sich hin. Die beiden Eisengewichte (Fichtenzapfen) von je 3,5 kg werden einmal in der Woche hochgezogen und liefern auf ihrer Talfahrt von 85 cm die nötige Energie für das Uhrwerk und den Antrieb des Kuckucks. Was ist die Leistung der Uhr?",
+    "type": "standard",
+    "block": "Kuckucksuhr",
+    "blocktext": "Eine Kuckucksuhr hängt an der Wand und tickt schon seit 250 Jahren vor sich hin. Die beiden Eisengewichte (Fichtenzapfen) von je 3,5 kg werden einmal in der Woche hochgezogen und liefern auf ihrer Talfahrt von 85 cm die nötige Energie für das Uhrwerk und den Antrieb des Kuckucks.",
+    "title": "Aufgabe 1",
+    "problem": "Was ist die Leistung der Uhr?",
+    "image": false,
     "given": [
-        { "letter": "Δt", "index": "1", "value": 1, "unit": "Wochen" },
+        { "letter": "Δt", "index": "1", "value": 604800, "unit": "Sekunden" },
         { "letter": "m",  "index": "Gewichte", "value": 7, "unit": "kg" },
-        { "letter": "h",  "index": "", "value": 85, "unit": "cm" },
-        { "letter": "g",  "index": "", "value": 9.81, "unit": "m/s²" },
+        { "letter": "h",  "index": "", "value": 0.85, "unit": "m" },
+        { "letter": "g",  "index": "", "value": 9.81, "unit": "m/s²" }
     ],
     "equations": [
         "P = E/Δt",
         "E = m*g*h"
+    ],
+    "solution": {
+        "letter": "P",
+        "index": "",
+        "value": 0.000096,
+        "unit": "Watt",
+        "formula": "P = m*g*h/Δt",
+        "formula_image": "solution1.png"
+    },
+    "units": [
+        "N*m/s",
+        "J*m/s²",
+        "m²/(s*kg)"
+    ],
+    "correct_solution_phrase": "Die Leistung, die zum Betrieb der Uhr benötigt wird, ist {{solution.value}} {{solution.unit}}.",
+    "alternative_solution_phrases": [
+        "Die Leistung der Uhr pro Woche ist {{solution.value}} {{solution.unit}}.",
+        "Die Energie der Uhr ist {{solution.value}} {{solution.unit}}.",
+        "Die Uhr ist 250 Jahre alt.",
+        "Die Uhr generiert {{solution.value}} {{solution.unit}} an Leistung"
     ]
 };
--- js/main.js
+++ js/main.js
@@ -1,4 +1,89 @@
+// MVC pattern
+var model = (function() {
+	var my = {};
+	my.getLives = function () {
+		var lives = localStorage.getItem("lives");
+		if (lives == undefined) {
+			lives = 3;
+			localStorage.setItem("lives", 3);
+		};
+		return lives;
+	};
+
+	my.subtractLife = function (lives) {
+		console.log("model.substractLife");
+		localStorage.setItem("lives", my.getLives()-1);
+	};
+
+	my.addLife = function (lives) {
+		console.log("model.addLife");
+		localStorage.setItem("lives", my.getLives()+1);
+	};
+
+	return my;
+}());
+
+// view
+var view = (function() {
+	var my = {};
+
+	my.init = function() {
+		$("#weiter").click(function(event) {
+			$(".hidden").removeClass("hidden");
+			$(".stepone").addClass("hidden");
+		});
+
+		$("#formel").click(function(event) {
+			myModule.addEquation("P = E/Δt");
+		});
+
+		myModule.init();
+		myModule.addObjects();
+
+		$("#addlife").click(controller.addLife(event));
+		$("#substractlife").click(controller.substractLife(event));
+	}
+
+	// evtl. Model als Argument
+	my.showLives = function() {
+		var lives = model.getLives();
+		console.log("showLives: lives = " + lives);
+		$("#lives").children().each(function (index) {
+			if (lives >= index + 1) {
+				$(this).removeClass().addClass("glyphicon glyphicon-heart");
+			} else{
+				$(this).removeClass().addClass("glyphicon glyphicon-heart-empty");
+			};
+		});
+	};
+	return my;
+}());
+
+
+// controller
+var controller = (function() {
+	var my = {};
+
+	my.addLife = function(event) {
+		console.log("controller.addLife");
+		model.addLife();
+		view.showLives();
+	};
+
+	my.subtractLife = function(event) {
+		console.log("controller.substractLife");
+		model.subtractLife();
+		view.showLives();
+	};
+	return my;
+}());
+
+
+// main
 $(document).ready(function(){
+	view.init();
+	// maybe put this in view and callbacks here?
+/*	$("#mvctest").click(controller.mvctest(event));
 	$("#pruefen").click(function(event) {
 		var correct = true;
 		$("#first :selected").each(function() {
@@ -12,18 +97,11 @@ $(document).ready(function(){
 		} else {
 			$("#result").removeClass().addClass("glyphicon glyphicon-remove");
 			$("#tmp").removeClass().addClass("glyphicon glyphicon-heart-empty");
-		};
-	});
-	$("#weiter").click(function(event) {
-		$(".hidden").removeClass("hidden");
-		$(".stepone").addClass("hidden");
-	});
-	$("#formel").click(function(event) {
-		myModule.addEquation("P = E/Δt");
-	});
-
-	myModule.init();
-	myModule.addObjects();
-
-	console.log(jsonObject);
+		};*/
+	
+	// JS templating
+	var template = $('#template').html();
+	Mustache.parse(template);   // optional, speeds up future uses
+	var rendered = Mustache.render(template, jsonObject);
+	$('#target').html(rendered);
 });
